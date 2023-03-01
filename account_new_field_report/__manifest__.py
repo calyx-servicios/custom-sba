ea141a26614c302af148ec48c61a4a0be7fe4a19
lucianobaleani <lucianobaleani94@gmail.com>
2023-03-01 14:29:20 -0300

    [ODOO-5618][UPD][account_new_field_report]Update the Manifest file

--- account_new_field_report/__manifest__.py
+++ account_new_field_report/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Account",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.1",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
